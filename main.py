f7fa66e9c5ab3d70badfb85b1d730477ccf26434
TheCoder777 <thecoder777.github@gmail.com>
2019-02-13 20:37:33 +0100

    fixed wrong branch, added .show() function

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import csv
 # ( x  /  y )
 # (cols/rows)
 class Table(object):
-    def __init__(self, name="New Table",  col=2, row=2, content=0):
+    def __init__(self, name="New Table",  col=2, row=2, content=0, root_id=0):
         self.name = name
         self.rows = row
         self.cols = col
         self.content = content
+        self.root_id = root_id
+        self.head = "Name:\t\t" + str(name) + "\nDimensions:\t" + str(self.cols) + "x" + str(self.rows) #define head variable
         self.build()
 
     def build(self): #'self.root' is the main table!
         self.root = [[self.content for col in range(self.cols) ]for row in range(self.rows)]
         # self.root = [[[[0,0]], [[0,0]]], [[[0,0]], [[0,0]]]] #example
 
-    def get(self, col=None, row=None): #needs an integer, or no value at all 
+    def get(self, col=None, row=None): #needs an integer, or no value at all
         try:
             # if col > self.cols or row > self.rows:
                 # print("Given col or row out of range/index!")
@@ -42,6 +44,34 @@ class Table(object):
         except:
             print("Given col or row isn't a number!")
             return self.root #returns the complete table if an error raises!
+
+    def show(self, head=True, body=True, root_id=True, brackets=True):
+        if head and root_id:
+            print("\n{}\nID:\t\t{}".format(self.head, self.root_id))
+        elif head and not root_id:
+            print("\n{}".format(self.head))
+        elif root_id and not head:
+            print("\nID:\t\t{}".format(self.root_id))
+        else:
+            pass #no head will be printed
+
+        if body:
+            print() #if head and root_id are both False, print only a newline for the body
+        else:
+             pass #if body is False, nothing here will be printed
+
+        if body:
+            if brackets:
+                for row in self.root:
+                    print(row) #print whole table with brackets and newlines
+            else: #brackets is now False, or anything other we don't know
+                for row in self.root:
+                    print()
+                    for col in row:
+                        print(col, end="") #without seperators
+                print()
+        else: #body is now False, or anything we don't care about
+            pass #body is False, so nothing here will be printed
 
 if __name__ == "__main__":
     pass
